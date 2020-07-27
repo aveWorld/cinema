1904aacbd946c08dfcccbbca2ac6a4b1633e0b91
aveWorld <texnarb228@gmail.com>
2020-07-27 20:33:45 +0300

    add some styles

--- src/App.js
+++ src/App.js
@@ -35,26 +35,33 @@ function App() {
     <div>
       <div className="inst">
         <h1>Here you can reserve sits like in the cinema</h1>
-        <div><span className="red-mark"></span> - means sits has been alredy reserved</div>
-        <div><span className="green-mark"></span> - means sits is empty</div>
+        <div><span className="blue-mark"></span> - means sits has been alredy reserved</div>
       </div>
-      {/* displaying out sits */}
-      <div className="sits">
-        {sits.map((e) => {
-          return (<div key={e.id} className="sit" style={e.reserved?{backgroundColor : "red"} : {backgroundColor: "green"}} onClick={(event) => reserveSit(event)}>{e.id}</div>)
-        })}
-      </div>
-      {/* displaying basket */}
       <button onClick={handleShow} className="btn btn-primary basket-btn">My Basket</button>
-      <div style={show?{display : "block"} : {display : "none"}}>
-        <div className="basket-header">
+      <div style={show?{display : "block"} : {display : "none"}} className="show-reservations">
+        <span className="exit-basket" onClick={handleShow}>x</span>
+        <h3 className="basket-header">
           You Ordered: 
-        </div>
+        </h3>
         {reserved.map((e,i) => {
           return (<div key={i}>
             <div className="order"><span className="order-number">{e}</span><button value={e} onClick={(event) => unreserveSit(event)} className="btn btn-danger">remove</button></div>
           </div>)
         })}
+      </div>
+      {/* displaying out sits */}
+      <div className="sits">
+        {sits.map((e) => {
+          return (<div key={e.id} className="sit" style={e.reserved?{backgroundColor : "#87CEFA"} : {backgroundColor: "white"}} onClick={(event) => reserveSit(event)}>{e.id}</div>)
+        })}
+      </div>
+      {/* displaying basket */}
+     
+      <div className="screen-3d">        
+          <div class="container-screen style-flat">  
+                <div class="flip"></div>
+          </div>
+        <p className="screen-text">screen</p>
       </div>
     </div>
   );
